6550e2d687e06bffe38f0ecb8c0aca3e66b9969f
eyramakua <eyramagginie@gmail.com>
2023-04-24 12:12:11 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int reverse;
+
+	for (reverse = 'z'; reverse >= 'a'; reverse--)
+	{
+		putchar(reverse);
+	}
+	putchar('\n');
+	return (0);
+}
