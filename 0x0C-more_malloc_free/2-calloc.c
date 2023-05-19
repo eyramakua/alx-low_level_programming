6bf7b87d80e70aac17b2d537723ccceebee3ca84
eyramakua <eyramagginie@gmail.com>
2023-05-18 18:25:56 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	bat = malloc(nmemb + size);
+	bat = malloc(nmemb * size);
 
 	if (bat != NULL)
 	{
