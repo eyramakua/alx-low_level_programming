aecc4b40fa5001af0657bc53d91a74a0b5518a68
eyramakua <eyramagginie@gmail.com>
2023-05-23 19:50:37 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - dog type
+ *
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
