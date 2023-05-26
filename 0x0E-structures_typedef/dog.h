2a24557df819cf8a4bc8320f273872a828912212
eyramakua <eyramagginie@gmail.com>
2023-05-26 02:47:38 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,11 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedef fpor struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,7 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
