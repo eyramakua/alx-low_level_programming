24a6afda52b7ce8d5b03766cfb17f4aacab14cd1
eyramakua <eyramagginie@gmail.com>
2023-09-08 09:52:45 -0700

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ * Return: Always (0)
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int h = 0;
+	int hub = 1;
+	hash_node_t *original = NULL;
+
+	if (ht == NULL)
+		return;
+	putchar('{');
+	for (h = 0; h < ht->size; h++)
+	{
+		if (ht->array[h])
+		{
+			original = ht->array[h];
+			while (original != NULL)
+			{
+				hub == 0 ? printf(", ") : hub;
+			       printf("'%s': '%s'", original->key, original->value);
+			       hub = 0;
+			       original = original->next;
+			}
+		}
+	}
+	printf("}\n");
+}
